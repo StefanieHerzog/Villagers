0488bc3024a8d0375102f9a0a74351d5315da8cf
Stefanie Herzog <stefanie.buechner@outlook.com>
2023-05-01 22:59:58 +0200

    Rules are printed at beginning of game

--- game.py
+++ game.py
@@ -17,6 +17,7 @@ class Game:
         answer = input()
         if answer.lower() == "yes":
             self.game_on = True
+            self.print_rules()
             self.create_first_villagers(8)
             self.sort_and_print_villagers_by_age()
             self.thread = threading.Thread(target=self.main_loop)
@@ -51,3 +52,16 @@ class Game:
     def sort_and_print_villagers_by_age(self):
         self.villagers = sorted(self.villagers, key=lambda villager: villager.age)
         self.print_stats_all_villagers()
+
+    def print_rules(self):
+        print("Gratulation! Du bist soeben der Verwalter eines kleinen Dorfes geworden. \nDeine Interaktionen werden darüber entscheiden, ob dein Dorf wächst oder eingeht.")
+        print("Arbeitsfähige Dorfbewohner kreieren Ressourcen, welche alle Dorfbewohner für ihre Gesundheit benötigen. \nWenn du keinen Dorfbewohner mehr hast, hast du verloren.")
+        print("Mit 40 Dorfbewohnern hast du gewonnen. \n Du kannst jederzeit folgende Befehle eingeben:")
+        print("show_stats()" ": zeigt dir den Status deines Dorfes und der Dorfbewohner")
+        print("marry(person1,person2)" ": verheiratet  zwei Dorfbewohner miteinander.")
+
+        # ToDo show how to get statistics of villagers and village
+
+        print("Verheiratete Paare versuchen automatisch jeden Monat, ein Kind zu bekommen. Erfolgschancen variieren je nach Alter, Gesundheit und Zufall.")
+        print("Die Ressourcen deines Dorfes beeinflussen die Gesundheit der Bewohner.Achtung, Minderjährige verbrauchen nur Ressourcen, aber schaffen keine")
+        print("Du kannst ausserdem bei Ereignissen über weitere Kommandos mit dem Spiel interagieren.")
